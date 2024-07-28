97584116b3cdbb849ae719f32f6d8dffd5cbdffb
JIUQI MAN <1467510054@qq.com>
2024-07-28 12:07:29 +0000

    ALL  Push

--- api/index.php
+++ api/index.php
@@ -1,9 +1,56 @@
+<?php
+include('./config.php');
+include('./function.php');
+error_reporting(0);
+try{
+    $pdo = pdoConnect();
+    $databaseQuery = databaseQuery($pdo, "code", str_replace("/","",$_GET['c']));
+} catch (Exception $e) {
+    ?>
+    <!DOCTYPE html>
+    <head>
+        <meta charset="UTF-8">
+        <style>
+            a{
+            text-decoration:none;
+            color:#4D4D4D;
+            }
+            .one{ font-weight: normal; }
+            .two{ font-weight: bold; }
+            .three{ font-weight: 200; }
+        </style>
+        <title><?php echo $TITLE?></title>
+    </head>
+    <body>
+        <center>
+            <h1 class="one">抱歉！出错啦！</h1>
+            <h2 class="three">连接数据库似乎出现了一个致命的错误</h2>
+        </center>
+    </body>
+    <?  
+    exit();
+}
+if($databaseQuery['num']!=0){
+    if($JUMP_TIME>0){
+        include('jump.php');
+        exit();
+    } else {
+        header('HTTP/1.1 301 Moved Permanently');
+        header('Location: '.$databaseQuery['result'][0]['url']);//
+    }
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
-    <title><?php echo "云电短链接" /></title>
+    <title><?php echo $TITLE?></title>
+    <link rel="stylesheet" href="//static.llilii.cn/css/other/background.css" />
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/mdui@1.0.0/dist/css/mdui.min.css" />
     <script src="https://static.llilii.cn/libs/jquery/3.5.1/jquery.min.js"></script>
     <style>
@@ -35,7 +82,7 @@
             <div class="mdui-card-content">
                 <div class="mdui-textfield">
                     <label class="mdui-textfield-label">需要生成短链的网址</label>
-                    <input class="mdui-textfield-input" id="url" placeholder="https://1v.fit" type="text" />
+                    <input class="mdui-textfield-input" id="url" placeholder="https://kagamine.top" type="text" />
                 </div>
                 <br>
             </div>
@@ -44,8 +91,9 @@
             </div>
         </div>
         <footer>
-            <!-- 本程序使用MIT协议开源，请遵守此协议，请勿删除本处版权，否则原作者保留一切法律权利 -->
-            <center><p style="color:white;">&copy; 2017-2024 Copyright <a style="color:white;" target="_blank" href="https://studio.yhdzz.cn/">云电工作室</a></p></center>
+            <!-- 本程序使用GPL2.0协议开源，请遵守此协议，请勿删除本处版权，否则原作者保留一切法律权利 -->
+            <!-- 如果看不懂GPL2.0协议请自行查看根目录人话版解释。如果想删除本处版权的请直接不要使用本程序。 -->
+            <center><p style="color:white;">&copy; 2020 Copyright <a style="color:white;" target="_blank" href="https://www.wunote.cn/">UnknownO</a></p></center>
         </footer>
 
     </div>
@@ -55,7 +103,7 @@
         function submit() {
             $("#submitbtn").attr("disabled", true);
             url = $("#url").val();
-            if (true) {
+            if (<?php if($IMAGE_VERIFICATION) echo 'true'; else echo 'false'; ?>) {
                 imageVerification(function(answer) {
                     request(url, answer)
                 })
