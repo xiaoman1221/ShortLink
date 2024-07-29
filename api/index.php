4a7243d3e7d70a09fddb7c7d5940baad8749b3e7
JIUQI MAN <1467510054@qq.com>
2024-07-29 17:48:51 +0000

    Test 4

--- api/index.php
+++ api/index.php
@@ -14,7 +14,7 @@ if ($_GET['init'] == getenv("INIT_SQL_PASSWORD")) {
     echo init($db);
 } elseif ($_GET['submit']) {
     $url = $_GET['submit'];
-    $query = "INSERT INTO url_data (id, url, code) VALUES (CURRENT_TIMESTAMP, '$url', '$randstr')";
+    $query = "INSERT INTO url_data (id, url, code) VALUES (time(), '$url', '$randstr')";
     $result = pg_query($db, $query);
     echo "成功！您的跳转链接为 http://test.com/jump={$randstr}";
 } elseif ($_GET['jump']) {
