ec6358a3e994441474788d977982b1b9c0721d3c
JIUQI MAN <1467510054@qq.com>
2024-07-29 17:11:51 +0000

    Init SQL Function

--- api/index.php
+++ api/index.php
@@ -4,11 +4,10 @@ $db = pg_connect("host=$DB_HOST dbname=$DB_NAME user=$DB_USER password=$DB_PASS"
 if (!$db) {
     die("无法连接到数据库");
 }
-print(GetRandStr($URL_SHORTENER_LENGHT));
-
-// $result = pg_query($db, $query);
-print($result);
-
+$randstr = GetRandStr($URL_SHORTENER_LENGHT);
+if ($_GET['init'] == "1145141*1"){
+    die(init());
+}
 function GetRandStr($length)
 {
     $str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
@@ -19,4 +18,13 @@ function GetRandStr($length)
         $randstr .= $str[$num];
     }
     return $randstr;
+}
+function init(){
+    $query = "CREATE TABLE `url_data` (
+  `id` int NOT NULL,
+  `url` text NOT NULL,
+  `code` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;";
+    $result = pg_query($db, $query);
+    return $result;
 }
