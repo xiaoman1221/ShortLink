1fd850eeff9ee0bbce265e642f8767c175fb28f4
JIUQI MAN <1467510054@qq.com>
2024-07-29 17:01:38 +0000

    Test Config File

---
+++ api/config.php
@@ -0,0 +1,15 @@
+<?php
+//数据库名
+$DB_HOST=getenv("POSTGRES_HOST");
+//数据库名
+$DB_NAME=getenv("POSTGRES_DATABASE");
+//数据库登录用户名
+$DB_USER=getenv("POSTGRES_USER");
+//数据库登录密码
+$DB_PASS=getenv("POSTGRES_PASSWORD");
+//几秒后跳转（等于0为关闭跳转页面）
+$JUMP_TIME=1;
+//站点名称
+$TITLE="云电短链";
+//生成短链的长度
+$URL_SHORTENER_LENGHT=8;
--- api/index.php
+++ api/index.php
@@ -1,28 +1,12 @@
 <?php
-//数据库名
-$DB_HOST=getenv("POSTGRES_HOST");
-//数据库名
-$DB_NAME=getenv("POSTGRES_DATABASE");
-//数据库登录用户名
-$DB_USER=getenv("POSTGRES_USER");
-//数据库登录密码
-$DB_PASS=getenv("POSTGRES_PASSWORD");
-//几秒后跳转（等于0为关闭跳转页面）
-$JUMP_TIME=1;
-//站点名称
-$TITLE="云电短链";
-//生成短链的长度
-$URL_SHORTENER_LENGHT=8;
-
+include('./config.php');
 $db = pg_connect("host=$DB_HOST dbname=$DB_NAME user=$DB_USER password=$DB_PASS");
-
 if (!$db) {
     die("无法连接到数据库");
 }
 print(GetRandStr($URL_SHORTENER_LENGHT));
 
-$query = "SELECT username, email FROM user";
-$result = pg_query($db, $query);
+// $result = pg_query($db, $query);
 print($result);
 
 function GetRandStr($length)
