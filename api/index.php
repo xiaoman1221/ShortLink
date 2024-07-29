f71d69ad64e9e8d56b8d0f10099a6555e6d6883a
JIUQI MAN <1467510054@qq.com>
2024-07-29 16:48:09 +0000

    2

--- api/index.php
+++ api/index.php
@@ -1,9 +1,26 @@
 <?php
+//数据库名
+$DB_HOST=getenv("POSTGRES_HOST");
+//数据库名
+$DB_NAME=getenv("POSTGRES_DATABASE");
+//数据库登录用户名
+$DB_USER=getenv("POSTGRES_USER");
+//数据库登录密码
+$DB_PASS=getenv("POSTGRES_PASSWORD");
+//几秒后跳转（等于0为关闭跳转页面）
+$JUMP_TIME=1;
+//站点名称
+$TITLE="云电短链";
+//生成短链的长度
+$URL_SHORTENER_LENGHT=111;
 
 
-print(GetRandStr(6));
+$db = pg_connect("host=$DB_HOST dbname=$DB_NAME user=$DB_USER password=$DB_PASS");
 
-
+if (!$db) {
+    die("无法连接到数据库");
+}
+print(GetRandStr($URL_SHORTENER_LENGHT));
 
 
 
